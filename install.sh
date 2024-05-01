9e1d20952e3fafad036c1ff2ec11456c7451cd75
Sebastian <foerster@forestsoft.de>
2024-05-01 22:03:15 +0200

    fix: make sure home is not empty

--- install.sh
+++ install.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
+if [ "$HOME" == "" ]; then
+  echo "Could not install dotfiles. Homedir is not recognized"
+  exit 2
+fi
+
 sudo apt update && sudo apt install -y curl nano rsync
 
-if [ ! -d ~/.fonts/ ]; then
-  mkdir -p ~/.fonts/
-  cd ~/.fonts/
+if [ ! -d $HOME/.fonts/ ]; then
+  mkdir -p $HOME/.fonts/
+  cd $HOME/.fonts/
   wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
   unzip JetBrainsMono.zip
   rm JetBrainsMono.zip
 fi
 
+
+
 if [ ! -x starship ]; then
-  mkdir -p ~/bin/
-  curl -sS https://starship.rs/install.sh | sh -s  -- --force --bin-dir ~/bin/
+  mkdir -p $HOME/bin/
+  curl -sS https://starship.rs/install.sh | sh -s  -- --force --bin-dir $HOME/bin/
 fi
 
 cd "$(dirname "${BASH_SOURCE}")";
@@ -26,8 +33,8 @@ function doIt() {
 		--exclude "install.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
-		-avh -O . ~;
-	source ~/.bash_profile;
+		-avh -O . $HOME;
+	source $HOME/.bash_profile;
 }
 
 if sh -c ": >/dev/tty" >/dev/null 2>/dev/null; then
