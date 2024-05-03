0db97e8da56883954d8951b9b3f0602f90cb2f9e
Sebastian <foerster@forestsoft.de>
2024-05-03 20:28:51 +0200

    Update install.sh
    fix syntax

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export GITHUB_USERNAME=${GITHUB_USERNAME:-"Forestsoft-de"}
-if [ ! -x command -v "chezmoi" ]; then
+if [ ! -x $(command -v "chezmoi") ]; then
 	sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
 fi
