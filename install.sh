bf58349c045d0d7c9e9c1bc067f740601c6827cd
Sebastian <foerster@forestsoft.de>
2024-05-03 20:48:50 +0200

    fix syntax

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
+set -e
 export GITHUB_USERNAME=${GITHUB_USERNAME:-"Forestsoft-de"}
 INSTALL_NEEDED=""
 if [ !-x $(command -v "jq") ]; then
   INSTALL_NEEDED="$INSTALL_NEEDED jq"
-  sudo apt update && sudo apt install -y jq
 fi
 if [ !-x $(command -v "curl") ]; then
   INSTALL_NEEDED="$INSTALL_NEEDED curl"
 fi
 
 if [ "$INSTALL_NEEDED" != "" ]; then
-	sudo apt update && sudo apt install -y $INSTALL_NEEDED
+  sudo apt update && sudo apt install -y $INSTALL_NEEDED
 fi
 
 if [ !-x $(command -v "chezmoi") ]; then
-	sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
+  sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
 fi
