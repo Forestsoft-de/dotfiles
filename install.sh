8f60d5c640946f98851351a5e97cff6afb1ba147
Sebastian <foerster@forestsoft.de>
2024-05-03 20:55:01 +0200

    Update install.sh
    fix condition v3

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ INSTALL_NEEDED=""
 if [ ! -x "$(command -v jq)" ]; then
   INSTALL_NEEDED="$INSTALL_NEEDED jq"
 fi
-if [ ! -x "$(command -v curl") ]; then
+if [ ! -x "$(command -v curl)" ]; then
   INSTALL_NEEDED="$INSTALL_NEEDED curl"
 fi
 
@@ -13,6 +13,6 @@ if [ "$INSTALL_NEEDED" != "" ]; then
   sudo apt update && sudo apt install -y $INSTALL_NEEDED
 fi
 
-if [ ! -x $(command -v "chezmoi") ]; then
+if [ ! -x "$(command -v chezmoi)" ]; then
   sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply "$GITHUB_USERNAME"
 fi
